125f266b7a53a3dc62f257e4880513e93454fbc6
Nalsolniel <thlapegue88@gmail.com>
2020-03-30 22:06:38 +0200

    test junit

--- pglp4_2/src/main/java/fr/uvsq/pglp4_2/operation/Division.java
+++ pglp4_2/src/main/java/fr/uvsq/pglp4_2/operation/Division.java
@@ -1,9 +1,15 @@
 package fr.uvsq.pglp4_2.operation;
 
 public class Division implements Operations{
-	public double exeOperation(final double operande1, final double operande2) {
+	public double exeOperation(final double operande1, final double operande2) throws Exception {
 		
-		return 0;
-	}
+	
 
+		if(operande2 == 0)
+		{
+			System.err.println("Il ets impossible de diviser par 0");
+			throw  new Exception();
+		}
+		return operande1 / operande2;
+	}
 }
--- pglp4_2/src/test/java/fr/uvsq/pglp4_2/AppTest.java
+++ pglp4_2/src/test/java/fr/uvsq/pglp4_2/AppTest.java
@@ -1,38 +1,73 @@
 package fr.uvsq.pglp4_2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import fr.uvsq.pglp4_2.operation.*;
+import fr.uvsq.pglp4_2.commandes.*;
+import java.util.Stack;
+
+import fr.uvsq.pglp4_2.commandes.Commandes;
+import fr.uvsq.pglp4_2.commandes.Undo;
+import fr.uvsq.pglp4_2.commandes.Quit;
+
+import fr.uvsq.pglp4_2.operation.Addition;
+import fr.uvsq.pglp4_2.operation.Soustraction;
+import fr.uvsq.pglp4_2.operation.Multiplication;
+import fr.uvsq.pglp4_2.operation.Division;
+import fr.uvsq.pglp4_2.operation.Operations;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+  
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+ 
+	@Test
+	public void TestUdo() {
+		Stack <Double> pile = new Stack<Double>();
+		Undo u = new Undo(pile);
+		pile.push(1.0);
+		u.actualisationPile();
+		u.exeCommande();
+		assertTrue(pile.size() == 0);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+	public void TestAddition() 
+	{
+		Addition a = new Addition();
+		assertTrue(a.exeOperation(1.0,1.0) == 2.0);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void Testoustraction() 
+	{
+		Soustraction a = new Soustraction();
+		assertTrue(a.exeOperation(2.0,1.0) == 1.0);
+	}
+
+	@Test
+	public void TestMultiplicartion() 
+	{
+		Multiplication a = new Multiplication();
+		assertTrue(a.exeOperation(2.0,1.0) == 2.0);
+	}
+
+	@Test(expected=Exception.class)
+	public void TestDivision() throws Exception
+	{
+		Division a = new Division();
+		assertTrue(a.exeOperation(4.0,2.0) == 2.0);
+	}
+
+
+
+
 }
